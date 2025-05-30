d7d55f367c2a3491b67eac609c4420433d35ed41
Andrey Isaev <andrey.isaev.54.ru@gmail.com>
2025-05-30 23:12:23 +0300

    feat: add table component

--- Frontend/src/components/Table/Table.tsx
+++ Frontend/src/components/Table/Table.tsx
@@ -15,17 +15,52 @@ export interface TableRecord {
   id: string,
   [key: string]: string|number|boolean|null,
 }
+
+export interface clickedRecordField {
+  id: string;
+  name: string;
+}
+
 export interface TableProps {
   fields: TableField[];
   records: TableRecord[];
+  onClick: (recordField: clickedRecordField) => void;
 }
 
 export const Table = (props: TableProps) => {
-
-  const { fields, records } = props;
+  const { fields, records, onClick } = props;
+  const fieldsNames = fields.map((field)=>field.name);
+  const recordsList = records.map((record)=>{
+    let resultRecord: TableRecord = {
+      id: record.id,
+    };
+    for(let name of fieldsNames) {
+      if(name in record) {
+        resultRecord[name] = record[name];
+      } else {
+        resultRecord[name] = "null";
+      }
+    }
+    return resultRecord;
+  })
+  
   return (
-    <div className={style.table}>
-    </div>
+    <section className={style.table}>
+      {
+        recordsList.map((record)=>{
+          const fields = Object.keys(record).map((key)=>{
+            const onClickField = () => {
+              onClick({id: record.id, name: key});
+            };
+            return ({
+              id: key,
+              element:<Field key={key} onClick={onClickField}value={record[key] ? record[key].toString() : "null"} />
+            });
+          })
+          return <RecordContainer key={record.id} fields={fields} />
+        })
+      }
+    </section>
   );
 }
 
